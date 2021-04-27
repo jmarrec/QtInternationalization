69ba42decbc1a3ef756ee413102ce72e69b87a04
Julien Marrec <julien.marrec@gmail.com>
2021-04-27 22:24:00 +0200

    Install the translator to french on the application

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.hpp"
 
 #include <QApplication>
+#include <QTranslator>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    if (translator.load("QtInternationalization_fr_FR")) {
+      a.installTranslator(&translator);
+    } else {
+        qDebug() << "Failed to load translator 'QtInternationalization_fr_FR'\n";
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
