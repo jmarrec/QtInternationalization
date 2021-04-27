63419c700164c70e1db6c4e7a3aaeb20a49080b0
Julien Marrec <julien.marrec@gmail.com>
2021-04-27 22:22:55 +0200

    Use Qt Linguist to translate the ts file (`~/Qt/6.0.3/gcc_64/bin/linguist`): very easy!

--- QtInternalization_fr_FR.ts
+++ QtInternalization_fr_FR.ts
@@ -1,3 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR"></TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Hello World!</source>
+        <translatorcomment>Being litteral here to test it out</translatorcomment>
+        <translation>Bonjour le Monde !</translation>
+    </message>
+</context>
+</TS>
