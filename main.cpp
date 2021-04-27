db757edb388a983013a98769e2ffdf298310b101
Julien Marrec <julien.marrec@gmail.com>
2021-04-27 22:35:27 +0200

    Lint, and bump to C++17 & Cmake 3.15

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+  QApplication a(argc, argv);
 
-    QTranslator translator;
-    if (translator.load("QtInternalization_fr_FR")) {
-      a.installTranslator(&translator);
-    } else {
-        qDebug() << "Failed to load translator 'QtInternalization_fr_FR'\n";
-    }
+  QTranslator translator;
+  if (translator.load("QtInternalization_fr_FR")) {
+    // Without this line, you see "Hello World!" instead
+    a.installTranslator(&translator);
+  } else {
+    qDebug() << "Failed to load translator 'QtInternalization_fr_FR'\n";
+  }
 
-    MainWindow w;
-    w.show();
-    return a.exec();
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,11 +3,11 @@
 #include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+  : QMainWindow(parent)
 {
-    auto center = new QPushButton(tr("Hello World!"), this);
-    center->setMinimumSize(400, 205);
-    setCentralWidget(center);
+  auto center = new QPushButton(tr("Hello World!"), this);
+  center->setMinimumSize(400, 205);
+  setCentralWidget(center);
 }
 
 MainWindow::~MainWindow()
