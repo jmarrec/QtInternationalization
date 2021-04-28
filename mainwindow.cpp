ee0bb47415fc874f2f5d02e3bd705fb61f6e4279
Julien Marrec <julien.marrec@gmail.com>
2021-04-28 11:02:30 +0200

    Add a button with a tr text

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.hpp"
 
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    auto center = new QPushButton(tr("Hello World!"), this);
+    center->setMinimumSize(400, 205);
+    setCentralWidget(center);
 }
 
 MainWindow::~MainWindow()
