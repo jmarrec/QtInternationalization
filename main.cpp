43c2c27233bbf1301971c338528840ac52353b74
Julien Marrec <julien.marrec@gmail.com>
2021-04-28 11:22:46 +0200

    Step 3: change translator to test it out

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
   QApplication a(argc, argv);
 
   QTranslator translator;
-  if (translator.load("QtInternationalization_fr_FR")) {
+  if (translator.load("QtInternationalization_es_ES")) {
     // Without this line, you see "Hello World!" instead
     a.installTranslator(&translator);
   } else {
-    qDebug() << "Failed to load translator 'QtInternationalization_fr_FR'\n";
+    qDebug() << "Failed to load translator 'QtInternationalization_es_ES'\n";
   }
 
   MainWindow w;
