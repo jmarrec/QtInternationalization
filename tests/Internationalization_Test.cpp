94b5f6db3e35143e05378cdf622c1f2c234bc99a
Julien Marrec <julien.marrec@gmail.com>
2021-04-28 13:50:49 +0200

    Extend test to menu bar as well

--- tests/Internationalization_Test.cpp
+++ tests/Internationalization_Test.cpp
@@ -25,16 +25,26 @@ void InternationalizationTest::testGui()
     QApplication::processEvents();
     QCOMPARE(m.m_button->text(), QString("\u00A1Hola mundo!. <FIXED VAL>. Otra frase traducible"));
     QCOMPARE(m.m_button->text(), QString("¡Hola mundo!. <FIXED VAL>. Otra frase traducible"));
-
+    QCOMPARE(m.m_langMenu->title(), QString("&Idioma"));
+    QCOMPARE(m.m_actionEN->text(), QString("Inglés"));
+    QCOMPARE(m.m_actionFR->text(), QString("Frances"));
+    QCOMPARE(m.m_actionES->text(), QString("Español"));
 
     m.m_actionFR->trigger();
     QApplication::processEvents();
     QCOMPARE(m.m_button->text(), QString("Bonjour le Monde !. <FIXED VAL>. Une autre phrase traduisible"));
+    QCOMPARE(m.m_langMenu->title(), QString("&Langue"));
+    QCOMPARE(m.m_actionEN->text(), QString("Anglais"));
+    QCOMPARE(m.m_actionFR->text(), QString("Français"));
+    QCOMPARE(m.m_actionES->text(), QString("Espagnol"));
 
     m.m_actionEN->trigger();
     QApplication::processEvents();
     QCOMPARE(m.m_button->text(), QString("Hello World!. <FIXED VAL>. Another translatable phrase"));
-
+    QCOMPARE(m.m_langMenu->title(), QString("&Language"));
+    QCOMPARE(m.m_actionEN->text(), QString("English"));
+    QCOMPARE(m.m_actionFR->text(), QString("French"));
+    QCOMPARE(m.m_actionES->text(), QString("Spanish"));
 }
 
 QTEST_MAIN(InternationalizationTest)
