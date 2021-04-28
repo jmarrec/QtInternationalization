3b54e071663df3a7aa5e6a8e198f313498fd29b3
Julien Marrec <julien.marrec@gmail.com>
2021-04-28 10:26:47 +0200

    Step 1: Register a new TS file and rebuild

---
+++ translations/QtInternalization_es_ES.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="8"/>
+        <source>Hello World!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
